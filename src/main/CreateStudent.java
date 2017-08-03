129386827d3678aba237edee0f6ecb36d2390c5a
Unknown <karlova1@gmail.com>
2017-08-03 16:45:00 +0300

    Create CreateStudent.class

---
+++ src/main/CreateStudent.java
@@ -0,0 +1,31 @@
+package main;
+
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateStudent {
+
+    public static void main(String[] args) {
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory()) {
+
+            Session session=factory.getCurrentSession();
+
+            System.out.println("Creating new student object...");
+            Student tempStudent=new Student("Dima","Pisa", "karlova1@gmail.com");
+
+            session.beginTransaction();
+
+            System.out.println("Saving the student...");
+            session.save(tempStudent);
+
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }
+    }
+}
